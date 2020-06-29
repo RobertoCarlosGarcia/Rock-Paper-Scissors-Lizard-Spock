07212d75e4baa8568065a4982306d4667516aa18
RobertoCarlosGarcia <66977916+RobertoCarlosGarcia@users.noreply.github.com>
2020-06-29 13:41:38 -0500

    Added displayRules function to the Game Class.

--- RPSLS.js
+++ RPSLS.js
@@ -9,7 +9,21 @@ class Game {
 
     functiongameGestures = ["Rock","Paper","Scissors","Lizard","Spock"];
     
-    
+displayRules() {
+    console.log ("Welcome to Rock, Paper,Scissors, Lizard, Spock Game!")
+    console.log ("First player to reach to 2 wins will be declared the winner.")
+    console.log ("Rock crushes Scissors.")
+    console.log ("Scissors cuts paper.")
+    console.log ("Paper covers Rock.")
+    console.log ("Rock crushes Lizard.")
+    console.log ("Lizard poisons Spock.")
+    console.log ("Spock smashes Scissors.")
+    console.log ("Scissors decapitates Lizard.")
+    console.log ("Lizard eats Paper.")
+    console.log ("Paper disproves Spock.")
+    console.log ("Spock vaporizes Rock.");
+
+    }
 
     } 
 }
@@ -31,4 +45,6 @@ class HumanPlayer extends Player{
 class AIPlayer extends Player{
 
 
+    }
+    
 }
